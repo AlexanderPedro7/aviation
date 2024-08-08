2a17585e887be3c8e40792ff909e74005d18a3e3
AlexanderPedro7 <alexanderpedro985@gmail.com>
2024-08-08 15:03:30 +0100

    Included Error Page

---
+++ src/pages/error.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const error = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center text-center' style={{ height:"70vh" }}>
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+    </div>
+  )
+}
+
+export default error
